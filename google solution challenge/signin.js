725d57b0d94f66e43be5bfedec212b8884253ef7
SIS301021 <scape.sids@gmail.com>
2023-03-19 01:05:06 +0530

    H

--- google solution challenge/signin.js
+++ google solution challenge/signin.js
@@ -11,20 +11,30 @@ const firebaseConfig = {
   // initialize firebase
 firebase.initializeApp(firebaseConfig);
 
-document.getElementById("submit").addEventListener("click", check());
-
-function check(){
+document.getElementById("submit").addEventListener("click", function check(){
     var database = firebase.database();
 
     var number = document.getElementById('signinEmail').value;
-    alert(number)
     database.ref('enteries').once('value', function(snapshot) {
               if (snapshot.hasChild(number)) {
+                var pass=document.getElementById()
+                database.ref('enteries').child(number).once("value" , function(snapshot){
+                  snapshot.forEach(function(element) {
+                    // if (element.key == 8979722289){
+                    //   data = element.val();
+                      // document.querySelector("#abc").innerHTML = 
+                      //   <div> ${element.val()}</div>
+                      // 
+                      // console.log(print(data));
+                      // console.log(element.key);
+                      if(element.key.value==pass)
+                        {
+                        }
+
                 alert("number registered")
                 }
-    else{
-        alert("number not registered")
-    }
+                }}
 });
-
+              
 }
+);
